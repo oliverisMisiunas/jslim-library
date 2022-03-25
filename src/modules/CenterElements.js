c914c62f76611566d57526fe0b9a33d4d99bb697
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2022-03-24 22:43:22 -0700

    add utility fn CenterElement

---
+++ src/modules/CenterElements.js
@@ -0,0 +1,22 @@
+//a utility fn for CenterCols and CenterRows
+function CenterElement(element) {
+  element.style.display = 'flex';
+  element.style.justifyContent = 'center';
+  element.style.alignItems = 'center';
+  return element;
+}
+
+function CenterElements() {
+  let flexCol = document.getElementsByClassName('jfcol');
+  let flexRow = document.getElementsByClassName('jfrow');
+
+  for (let i = 0; i < flexCol.length; i++) {
+    let col = CenterElement(flexCol[i]);
+    col.style.flexDirection = 'column';
+  }
+  for (let i = 0; i < flexRow.length; i++) {
+    let row = CenterElement(flexRow[i]);
+  }
+}
+
+export default CenterElements;
--- src/plugin/JSlim.js
+++ src/plugin/JSlim.js
@@ -1,14 +1,14 @@
-import AddOnClick from "../modules/AddOnClick.js";
-import CenterElements from "../modules/CenterElement.js";
-import ClassOnHover from "../modules/ClassOnHover.js";
-import CreateVariable from "../modules/CreateVariable.js";
-import DisplayDate from "../modules/DisplayDate.js";
-import PushToArray from "../modules/PushToArray.js";
-import RemoveOnClick from "../modules/RemoveOnClick.js";
-import ScrollToPos from "../modules/ScrollToPos.js";
-import ScrollToTop from "../modules/ScrollToTop.js";
-import ToggleOnClick from "../modules/ToggleOnClick.js";
-import ToggleOnScroll from "../modules/ToggleOnScroll.js";
+import AddOnClick from '../modules/AddOnClick.js';
+import CenterElements from '../modules/CenterElements.js';
+import ClassOnHover from '../modules/ClassOnHover.js';
+import CreateVariable from '../modules/CreateVariable.js';
+import DisplayDate from '../modules/DisplayDate.js';
+import PushToArray from '../modules/PushToArray.js';
+import RemoveOnClick from '../modules/RemoveOnClick.js';
+import ScrollToPos from '../modules/ScrollToPos.js';
+import ScrollToTop from '../modules/ScrollToTop.js';
+import ToggleOnClick from '../modules/ToggleOnClick.js';
+import ToggleOnScroll from '../modules/ToggleOnScroll.js';
 
 function JSlim() {
   CenterElements();
